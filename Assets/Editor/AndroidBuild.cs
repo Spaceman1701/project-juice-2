dc8fc03e838d209fd66850413332aa7ef9f8cc1f
Ethan Hunter <fc.spaceman@gmail.com>
2020-02-07 21:50:26 -0700

    add autobuilder

---
+++ Assets/Editor/AndroidBuild.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AndroidBuild : MonoBehaviour
+{
+
+    [MenuItem("AutoBuilder/Quest")]
+    static void PerformBuild()
+    {
+        string[] scenes = { "Assets/Scenes/MainScene.unity" };
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = scenes;
+        buildPlayerOptions.locationPathName = "./thebuild";
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.AutoRunPlayer;
+
+
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
